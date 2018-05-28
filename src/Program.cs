c91f40ca0d545ed2faf7615ae467aaff91c66b90
John Wiggins <lilwiggy123@gmail.com>
2018-05-28 17:30:23 -0400

    Don't leak your phone number this time
    Hopefully...

---
+++ src/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace PennySharp
+{
+    class Program
+    {
+        private CommandService commands;
+        private CommandServiceConfig config;
+        private DiscordSocketClient client;
+        private IServiceProvider services;
+
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync ()
+        {
+
+            client = new DiscordSocketClient();
+            config = new CommandServiceConfig()
+            {
+                CaseSensitiveCommands = false,
+                ThrowOnError = true,
+                DefaultRunMode = RunMode.Async
+            };
+            commands = new CommandService(config);
+
+            // Events
+
+            client.Log += Log;
+            client.Ready += Ready;
+
+            services = new ServiceCollection()
+            .BuildServiceProvider();
+
+            await InstallCommands();
+
+            await client.LoginAsync(TokenType.Bot, "Heck you bro");
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+
+        }
+        
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        private Task Ready()
+        {
+            client.SetStatusAsync(UserStatus.DoNotDisturb);
+            client.SetGameAsync("Not combat ready.");
+            return Task.CompletedTask;
+        }
+
+
+        public async Task InstallCommands()
+        {
+            client.MessageReceived += HandleCommand;
+
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+        }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            var msg = messageParam as SocketUserMessage;
+            if (msg == null) return;
+
+            int argsPos = 0;
+
+            if (!(msg.HasCharPrefix('!', ref argsPos) || msg.HasMentionPrefix(client.CurrentUser, ref argsPos))) return;
+
+            var ctx = new CommandContext(client, msg);
+
+            var res = await commands.ExecuteAsync(ctx, argsPos, services);
+            if (!res.IsSuccess)
+                Console.WriteLine(res.ErrorReason);
+        }
+
+
+    }
+}
---
+++ src/commands.cs
@@ -0,0 +1,50 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+public class Tests : ModuleBase
+{
+    [Command("test"), Summary("Just test shit")]
+    public async Task Test()
+    {
+        await ReplyAsync("Sup faggot");
+    }
+
+    [Command("ping"), Summary("Just test shit")]
+    public async Task Pong()
+    {
+        await ReplyAsync("Fuck you");
+    }
+
+    [Command("danganronpa"), Summary("Yes")]
+    public async Task Danganronpa()
+    {
+        Discord.Embed emb = new Discord.EmbedBuilder() {
+            Author = new Discord.EmbedAuthorBuilder() {
+                IconUrl = "https://cdn.discordapp.com/avatars/309531399789215744/5d27d9ce0a36adf52d103f8c7c3a9cfa.png?size=2048",
+                Name = "Geoff",
+                Url = "https://discord.gg/kwcd9dq"
+            },
+
+            ImageUrl = "https://vignette.wikia.nocookie.net/danganronpa/images/3/32/DR3_Blue_Ray_Box_002.png/revision/latest?cb=20161127185631"
+
+        };
+        await ReplyAsync("", false, emb);
+    }
+}
+
+    [Group("dang")]
+public class Things : ModuleBase
+{
+    [Command("man"), Summary("ron")]
+    public async Task Pa()
+    {
+        await ReplyAsync("ron pa");
+    }
+
+    [Command("man ron"), Summary("ron")]
+    public async Task Ron()
+    {
+        await ReplyAsync("pa");
+    }
+}
+
